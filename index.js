aadbefbf3b06c87aa66baa2ebc7540701c3d5c9b
DaniNere <danisilvanere@gmail.com>
2024-03-01 11:04:18 -0300

    Created routers categories, orders and products

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require("express");
-const app = express(); 
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 require('dotenv').config();
+const app = express(); 
 
-const userRoute = require('./routes/user');
+const categoriesRoute = require('./routes/categories');
+const productRoute = require('./routes/products');
+const userRoute = require('./routes/users');
+const orderRoute = require('./routes/orders');
+
 
 // Middleware
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.DATABASE_URL).then(() => console.log("Banco Conectado")).catch(err => console.log("Erro ao conectar ao banco de dados"));
+mongoose.connect(process.env.DATABASE_URL).then(() => 
+console.log("Banco Conectado")).catch(err => console.log("Erro ao conectar ao banco de dados"));
 
 app.use('/api', userRoute);
+app.use(`${api}/products`, productRoute);
+app.use(`${api}/categories`, categoriesRoute);
+app.use(`${api}/users`, userRoute);
+app.use(`${api}/orders`, orderRoute);
 
 app.listen(3000, () => {
     console.log("O servidor está rodando em http://localhost:3000");
