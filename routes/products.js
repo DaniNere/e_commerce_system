2ece76105ba65526cb7879418b9da215af69ae55
DaniNere <danisilvanere@gmail.com>
2024-03-12 14:40:20 -0300

    :sparkles:Create product update function

--- routes/products.js
+++ routes/products.js
@@ -45,6 +45,39 @@ router.post("/", async (req, res) => {
       res.status(500).json({ success: false, error: error.message });
     }
   });
+
+  router.put("/:id", async(req, res) =>{
+
+    try{
+        const category = await Category.findById(req.body.category);
+
+    if (!category) {
+  return res.status(400).json({ success: false, message: 'Invalid category' });
+    }
+
+    const product = await Product.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
+        image: req.body.image,
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured
+        
+    }, {
+        new: true
+    });
+
+    return res.status(200).json({ success: true, message: 'Product updated successfully', product });
+
+    }catch(error){
+        return res.status(500).json({ success: false, message: error.message });
+    }
+  })
   
   module.exports = router;
 
