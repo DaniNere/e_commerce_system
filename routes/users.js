957bf23b9f6118cf3006bbd49214ca4bb1235928
DaniNere <danisilvanere@gmail.com>
2024-03-13 17:17:05 -0300

    :sparkles:Peering jwt

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require('../models/user');
 const {hashPassword} = require('../utils/passwordUtils');
+const jwt = require("jsonwebtoken");
 const bcrypt = require('bcryptjs');
 
 
